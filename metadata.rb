ace52e67a8b3dc7c0f75a776cccbbd1087494951
Alexander Merkulov <sasha@merqlove.ru>
2018-04-17 02:07:46 +0300

    fix user; bump 0.1.9

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sasha@merqlove.ru'
 license          'MIT'
 description      'Installs/Configures dante'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.8'
+version          '0.1.9'
 
 supports 'centos'
 
