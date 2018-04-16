b261835c9ed2c7fa8db9e703004a28853b6c65ab
Alexander Merkulov <sasha@merqlove.ru>
2018-04-17 01:04:54 +0300

    Fix bugs. Bump 0.1.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['dante_ng']['user'] = 'username'
+default['dante_ng']['username'] = 'username'
 default['dante_ng']['password'] = '$1$4GSegYQW$XNIOjydYHkiLl3.R8MTd5/'
 default['dante_ng']['packages'] = %w(dante-server dante)
 
@@ -6,17 +6,14 @@ default['dante_ng']['config_path'] = '/etc/sockd.conf'
 
 default['dante_ng']["config"]['socksmethod'] = 'username'
 default['dante_ng']["config"]['logoutput'] = 'stderr'
-default['dante_ng']["config"]['internals'] = [{
-  interface: "eth0",
-  port: 57500
-}, {
-  interface: "127.0.0.1",
-  port: 57500
-}]
+default['dante_ng']["config"]['internals'] = {
+  "eth0": 57500,
+  "127.0.0.1": 57500
+}
 default['dante_ng']["config"]['external'] = 'eth0'
 default['dante_ng']["config"]['socksmethod'] = 'username'
 default['dante_ng']["config"]['user.privileged'] = 'root'
-default['dante_ng']["config"]['user.notprivileged'] = 'sockd'
+default['dante_ng']["config"]['user.notprivileged'] = node['dante_ng']['user']
 default['dante_ng']["config"]['client_pass'] = {
   from: "0.0.0.0/0",
   to: "0.0.0.0/0",
